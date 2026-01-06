009278409b7c361eab3964fe13e4402d27c9585d
Henry Ngai <Henryngai98@gmail.com>
2026-01-05 23:39:19 -0500

    Tinkering with slices

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 )
 
-func main(){
+type Job struct {
+	/*
+		Note
+		Struct tag used for JSON serialization. This is basically saying when
+		this struct is converted to JSON, the field ID should be represented
+		with the key "id".
+	*/
+
+	ID   int    `json:"id"`
+	Data string `json:"data"`
+}
+
+func createDummyJobs() []Job {
+	jobs := []Job{
+		{ID: 1, Data: "First Job"},
+		{ID: 2, Data: "Second Job"},
+		{ID: 3, Data: "Third Job"},
+	}
+	return jobs
+}
+
+func main() {
 	log.Println("Producer started")
+	firstJob := Job{ID: 1, Data: "Sample Data"}
+	log.Printf("Created job: %+v\n", firstJob)
+	jsonByteSlice, err := json.Marshal(firstJob)
+	if err != nil {
+		log.Fatalf("Failed to marshal job: %v", err)
+	}
+	log.Printf("JSON data: %s\n", jsonByteSlice)
 }
